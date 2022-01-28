915b3be1deebecebdfe96b8cf1f07f8133b7885f
adamhinton <adam.d.hinton@gmail.com>
2022-01-27 22:06:52 -0500

    first post auth register test passing

--- api/server.test.js
+++ api/server.test.js
@@ -2,7 +2,25 @@ const request = require("supertest");
 const server = require("./server");
 const db = require("../data/dbConfig");
 
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+afterAll(async () => {
+  await db.destroy();
+});
+
 // Write your tests here
 test("sanity", () => {
-  expect(true).toBe(false);
+  expect(true).toBe(true);
+});
+
+describe("POST auth register", () => {
+  test("adds new user", async () => {
+    const res = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "123", password: "456" });
+    console.log("res:", res);
+    expect(res.status).toBe(200);
+  });
 });
