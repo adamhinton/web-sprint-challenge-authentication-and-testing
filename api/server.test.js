91b9b7244a8898a0bd5fe52384f9a98897a63059
adamhinton <adam.d.hinton@gmail.com>
2022-01-27 21:55:51 -0500

    imported requirements for server.test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,8 @@
+const request = require("supertest");
+const server = require("./server");
+const db = require("../data/dbConfig");
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("sanity", () => {
+  expect(true).toBe(false);
+});
